4f98238312f283090ee9a6b3b02ede08a2f42ad7
Micah Esmond <mesmond@gmail.com>
2015-11-14 20:18:49 -0500

    Updated fluid2D Class

--- include/fluids2D.h
+++ include/fluids2D.h
@@ -3,7 +3,7 @@
 /*
  * File: fluids2D.h
  * Description: This file contains declarations and definitions of
- * 	several useful functions related to fluids.  This includes
+ * 	several useful functions and classes related to fluids.  This includes
  * 	relevant spacial arrays and operations.
 */
 //***************************************************************************
@@ -14,6 +14,7 @@
 #include <iostream>
 
 #include "dataTypes2D.h"
+#include "simulationConstants.h"
 
 using namespace std;
 
@@ -42,16 +43,26 @@ class PerfectGas2D
 	SpacialArray2D<double> totalEnergy_rhs;
 
 	//Properties*************************************************************
-	SpacialArray2D<double> soundSpeed;	//m/s
+	SpacialArray2D<double> soundSpeed;			//m/s
+	SpacialArray2D<double> thermalConductivity;	//W/(m K)
+	SpacialArray2D<double> thermalDiffusivity;	//m^2/s
+	SpacialArray2D<double> dynamicViscosity;	//Pa s
+	SpacialArray2D<double> kinematicViscosity;	//m^2/s
+
 
 	double gamma;						//Specific Heat Ratio (constant)
-	double particleMass;				//kg
+	double particleMass;				//kg/particle
+	double particleRadius;				//m
+	double molarWeight;					//kg/mol
+
 	double maxSoundSpeed;				//m/s
 
 	public:
 	PerfectGas2D(
 		Vector2D<int> size=Vector2D<int>(10,10),
-		double particleMass=2.66e-26, double gamma=1.6667)
+		double gamma=5.0/3.0,
+		double particleMass=2.3258671e-26,
+		double particleRadius=65.0e-12)
 		:
 			size(size),
 			//Dependent Variables********************************************
@@ -69,11 +80,106 @@ class PerfectGas2D
 			totalEnergy_rhs(size),
 			//Properties*****************************************************
 			soundSpeed(size),
+			thermalConductivity(size),
+			thermalDiffusivity(size),
+			dynamicViscosity(size),
 			gamma(gamma),
-			particleMass(particleMass) {}
+			particleMass(particleMass),
+			particleRadius(particleRadius) {}
 
-	void print_massDensity() const { massDensity.print(); }
+	inline Vector2D<int> getSize() const { return size; }
+
+	inline void print_molarDensity() const { molarDensity.print(); }
+	inline void print_massDensity() const { massDensity.print(); }
+
+	//***********************************************************************
+	//Thermal Speeds*********************************************************
+	/*
+	 * 	Function: thermalVelocity_avg()
+	 * 	Purpose: Return the maxwellian averaged thermal velocity.
+	 */
+	inline double thermalVelocity_avg(int i, int j) const
+		{ return sqrt(8.0*c_k*temperature.get(i,j)/(c_pi*particleMass) ); }
+
+	/*
+	 * 	Function: thermalVelocity()
+	 * 	Purpose: Return the maxwellian thermal velocity.  This is the
+	 * 		most probable speed of an individual particle in the gas
+	 */
+	inline double thermalVelocity(int i, int j) const
+		{ return sqrt(2.0*c_k*temperature.get(i,j)/particleMass ); }
+
+	/*
+	 * 	Function: thermalVelocity_rms()
+	 * 	Purpose: Return the RMS value of the velocity for a
+	 * 		maxwellian distribution.
+	 */
+	inline double thermalVelocity_rms(int i, int j) const
+		{ return sqrt(3.0*c_k*temperature.get(i,j)/particleMass ); }
+
+
+	//***********************************************************************
+	//Collisions*************************************************************
+
+	/*
+	 * 	Function: collFreq()
+	 * 	Purpose: Return the self collision frequency for the fluid assuming
+	 * 		electrically neutral.
+	 */
+	inline double collFreq(int i, int j)
+	{
+		//Woods (1993) (pg. 40)
+		return 1.414213562*c_pi*molarDensity.get(i,j)*c_Avagadro
+			*thermalVelocity_avg(i,j)
+			*4.0*particleRadius*particleRadius;
+	}
+
+
+	//***********************************************************************
+	//Derived Properties*****************************************************
+	/*
+	 * 	Function: getThermalConductivty()
+	 *	Purpose: Return the thermal conductivity based on
+	 * 		self collisions only and assuming electrically neutral.
+	 */
+	inline double getThermalConductivty(int i, int j)
+	{
+		//See Bukowski (1996).
+		return (5.0/2.0)*molarDensity.get(i,j)*c_Avagadro
+			*c_k*c_k*temperature.get(i,j)
+			/(particleMass*collFreq(i,j)); // [W m^{-1} K{-1}]
+	}
+
+	/*
+	 * Function: getSoundSpeed()
+	 * Purpose: Return the local fluid sound speed.
+	 */
+	inline double getSoundSpeed(int i, int j)
+	{
+		return sqrt(gamma*c_k*temperature.get(i,j)/particleMass);
+	}
+	
+
 };
+
+
+//~ class ThreeComponentPlasma2D
+//~ {
+	//~ private:
+	//~ Vector2D<int> size;
+	//~ 
+	//~ PerfectGas2D elec;
+	//~ PerfectGas2D ions;
+	//~ PerfectGas2D neut;
+//~ 
+	//~ public:
+	//~ ThreeComponentPlasma2D(
+		//~ Vector2D<int> size=Vector2D<int>(10,10),
+		//~ 
+		//~ double gamma=5.0/3.0,
+		//~ double particleMass=2.3258671e-26,
+		//~ double particleRadius=65.0e-12)
+//~ }
 
 
 #endif // INCLUDE_FLUIDS2D_H_
